015b9ce31a08d419e8501b1f4a8c443b1452f58f
tmcaldwell11 <tmcaldwell11@gmail.com>
2020-12-14 19:54:23 -0500

    adding initial files

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var app = new Vue ({
+   el: '#app',
+   data: {
+       // insert text for header and image text
+       Header: 'CS601 HW6 using Vue.js featuring my dog Evie',
+       imageText1: 'Evie as a baby in 2014.',
+       imageText2: 'Evie and I on her 2nd Birthday',
+       imageText3: 'Me, Evie, and my sister Katrina',
+       imageText4: '2020 Christmas Evie',
+
+       // insert images to each card
+       image1: 'images/image-one.jpg',
+       image2: 'images/image-three.jpg',
+       image3: 'images/image-five.jpg',
+       image4: 'images/image-seven.jpg',
+
+       
+       img1: 'images/image-one.jpg',
+       img2: 'images/image-two.jpg',
+       img3: 'images/image-three.jpg',
+       img4: 'images/image-four.jpg',
+       img5: 'images/image-five.jpg',
+       img6: 'images/image-six.jpg',
+       img7: 'images/image-seven.jpg',
+       img8: 'images/image-eight.jpg',
+     }
+ })
